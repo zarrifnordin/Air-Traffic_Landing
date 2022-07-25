b6d8a7849c52632528c6517d010af8996665d900
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-25 21:30:56 +0800

    Update SFO_Traffic.py

--- SFO_Traffic.py
+++ SFO_Traffic.py
@@ -12,8 +12,8 @@ pd.set_option('mode.chained_assignemnt' , None)
 
 data = pd.read_csv("/kaggle/input/sf-air-traffic-landing-statistics/Air_Traffic_Landings_Statistics.csv")
 
-df["Aircraft Version].replace('-', np.nan, inplace = True)
+data["Aircraft Version].replace('-', np.nan, inplace = True)
    
-df.head()
+data.head()
    
 
