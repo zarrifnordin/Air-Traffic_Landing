a6ccc50a278499b3b55f578885b147290ec5b457
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-25 21:48:07 +0800

    Update SFO_Traffic.py

--- SFO_Traffic.py
+++ SFO_Traffic.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
+for dirname, _, filenames in os.walk('/kaggle/input/sf-air-traffic-landing-statistics/Air_Traffic_Landings_Statistics.csv'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
