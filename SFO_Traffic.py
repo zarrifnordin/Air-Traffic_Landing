320399f965ba95d4418a97ad3e46fbb332a6d2a6
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-25 21:21:23 +0800

    Update SFO_Traffic.py

--- SFO_Traffic.py
+++ SFO_Traffic.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("")
+data = pd.read_csv("https://raw.githubusercontent.com/zarrifnordin/Air-Traffic_Landing/main/Air_Traffic_Landings_Statistics.csv")
+
+df["Aircraft Version'].replace('-', np.nan, inplace = True)
+   
+df.head()
