19fa29403c099f464a67f550f3dabd205c418d3e
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-25 21:30:00 +0800

    Update SFO_Traffic.py

--- SFO_Traffic.py
+++ SFO_Traffic.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("https://raw.githubusercontent.com/zarrifnordin/Air-Traffic_Landing/main/Air_Traffic_Landings_Statistics.csv")
+import os
+for dirname, _, filenames in os.walk('/kaggle/input') :
+   for filename in filenames:
+      print(os.path.join(dirname, filename))
+      
+import matplotlib.pyplot as plt
 
-df["Aircraft Version'].replace('-', np.nan, inplace = True)
+pd.set_option('mode.chained_assignemnt' , None)
+
+data = pd.read_csv("/kaggle/input/sf-air-traffic-landing-statistics/Air_Traffic_Landings_Statistics.csv")
+
+df["Aircraft Version].replace('-', np.nan, inplace = True)
+   
+df.head()
    
 
