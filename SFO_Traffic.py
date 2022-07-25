18e35f6598f9b7f0700497e162ac400a97c1804c
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-25 21:49:07 +0800

    Update SFO_Traffic.py

--- SFO_Traffic.py
+++ SFO_Traffic.py
@@ -12,20 +12,6 @@ import seaborn as sns
 pd.set_option('mode.chained_assignment', None)
    
 
-import numpy as np 
-import pandas as pd
 
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-pd.set_option('mode.chained_assignment', None)
 
 
