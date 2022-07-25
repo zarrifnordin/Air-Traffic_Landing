d0785d9f4f6b90bef04daaef010f8e6b639567f5
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-25 21:35:24 +0800

    Update SFO_Traffic.py

--- SFO_Traffic.py
+++ SFO_Traffic.py
@@ -1,19 +1,31 @@
-import numpy as np
+import numpy as np 
 import pandas as pd
 
 import os
-for dirname, _, filenames in os.walk('/kaggle/input') :
-   for filename in filenames:
-      print(os.path.join(dirname, filename))
-      
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-pd.set_option('mode.chained_assignemnt' , None)
-
-df = pd.read_csv("/kaggle/input/sf-air-traffic-landing-statistics/Air_Traffic_Landings_Statistics.csv")
-
-df["Aircraft Version"].replace('-', np.nan, inplace = True)
+pd.set_option('mode.chained_assignment', None)
    
-df.head()
-   
+
+import numpy as np 
+import pandas as pd
+
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+pd.set_option('mode.chained_assignment', None)
+
 
